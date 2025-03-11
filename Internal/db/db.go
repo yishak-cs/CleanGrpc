8475da7ed85e85e890c1706869352ff51952e2d8
yishak <yishakab24@gmail.com>
2025-03-11 10:27:29 +0300

    here is auto migrate (;>)

--- Internal/db/db.go
+++ Internal/db/db.go
@@ -2,6 +2,8 @@ package db
 
 import (
 	"log"
+
+	"github.com/yishak-cs/CleanGrpc/Internal/model"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -10,6 +12,7 @@ func DBconn() *gorm.DB {
 	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("There was error connecting to the database: %v", err)
-	  }
-	  return db
+	}
+	db.AutoMigrate(&model.User{})
+	return db
 }
